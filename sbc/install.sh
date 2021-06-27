867c98c9d3996ab9746b284278a2633b229c6afd
gilphilbert <gil.philbert@gmail.com>
2021-06-26 22:49:55 -0700

    Update install.sh

--- sbc/install.sh
+++ sbc/install.sh
@@ -3,16 +3,17 @@
 #-----------------------------------------
 # Install script for Hypnic Power Manager
 #-----------------------------------------
-
+CYAN='\033[0;36m'
+NC='\033[0m'
 echo ""
 echo ""
-echo "██╗░░██╗██╗░░░██╗██████╗░███╗░░██╗██╗░█████╗░"
-echo "██║░░██║╚██╗░██╔╝██╔══██╗████╗░██║██║██╔══██╗"
-echo "███████║░╚████╔╝░██████╔╝██╔██╗██║██║██║░░╚═╝"
-echo "██╔══██║░░╚██╔╝░░██╔═══╝░██║╚████║██║██║░░██╗"
-echo "██║░░██║░░░██║░░░██║░░░░░██║░╚███║██║╚█████╔╝"
-echo "╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝░░╚══╝╚═╝░╚════╝░"
-echo ""
+echo -e "${CYAN}██╗░░██╗██╗░░░██╗██████╗░███╗░░██╗██╗░█████╗░"
+echo -e "${CYAN}██║░░██║╚██╗░██╔╝██╔══██╗████╗░██║██║██╔══██╗"
+echo -e "${CYAN}███████║░╚████╔╝░██████╔╝██╔██╗██║██║██║░░╚═╝"
+echo -e "${CYAN}██╔══██║░░╚██╔╝░░██╔═══╝░██║╚████║██║██║░░██╗"
+echo -e "${CYAN}██║░░██║░░░██║░░░██║░░░░░██║░╚███║██║╚█████╔╝"
+echo -e "${CYAN}╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝░░╚══╝╚═╝░╚════╝░"
+echo -e "${NC}
 echo ""
 
 if [ "$EUID" -ne 0 ]
@@ -22,13 +23,26 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+DIR=""
+
+for LOC in "/lib" "/usr/lib"; do
+  if [[ -f "$LOC/systemd/systemd-shutdown" ]]; then
+    DIR="/usr/lib/systemd/"
+  fi
+done
+
+if [[ $DIR == "" ]]; then
+  echo "Could not find your systemd installation. Hypnic is currently only supported on systems running systemd"
+  exit
+fi
+
 echo "Downloading and installing application..."
 wget -qO /etc/hypnic.conf https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.conf
 wget -qO /usr/bin/hypnic.py https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.py
-wget -qO /etc/systemd/system/hypnic.service https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.service
-wget -qO /etc/systemd/system-shutdown/hypnic-shutdown.py https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic-shutdown.py
+wget -qO "$DIR/systemd/system/hypnic.service" https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.service
+wget -qO "$DIR/systemd/system-shutdown/hypnic-shutdown.py" https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic-shutdown.py
 chmod 755 /usr/bin/hypnic.py
-chmod 755 /usr/lib/systemd/system-shutdown/hypnic-shutdown.py
+chmod 755 "$DIR/systemd/system-shutdown/hypnic-shutdown.py"
 
 echo "Configuring service..."
 systemctl daemon-reload
