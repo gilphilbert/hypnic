58fce9273666085360d457ce8857c598dc373f3a
gilphilbert <gil.philbert@gmail.com>
2021-08-11 14:10:21 -0700

    pcp support

--- sbc/install.sh
+++ sbc/install.sh
@@ -22,33 +22,54 @@ if [ "$EUID" -ne 0 ]
 fi
 
 DIR=""
+MODE="fail"
 
 for LOC in "/lib" "/usr/lib"; do
   if [[ -f "$LOC/systemd/systemd-shutdown" ]]; then
     DIR="$LOC/systemd"
+    MODE="systemd"
+    echo "systemd-based system detected at $DIR"
   fi
 done
 
-if [[ $DIR == "" ]]; then
-  echo "Could not find your systemd installation. Hypnic is currently only supported on systems running systemd"
-  exit
+if [[ -f /etc/os-release ]]; then
+  if grep -Fq "piCorePlayer" /etc/os-release; then
+    echo "PiCorePlayer system detected"
+    MODE="pcp"
+  fi
 fi
 
-echo "Installing required libraries..."
-DEBIAN_FRONTEND=noninteractive apt-get install -qq python3-dev python3-rpi.gpio < /dev/null > /dev/null
+if [[ "$MODE" == "fail" ]]; then
+  echo "Could not detect your OS. Please open a ticket at https://github.com/gilphilbert/hypnic/issues"
+  exit 1
+fi
 
-echo "Downloading and installing application..."
-wget -qO /etc/hypnic.conf https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.conf
-wget -qO /usr/bin/hypnic.py https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.py
-wget -qO "$DIR/system/hypnic.service" https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.service
-wget -qO "$DIR/system-shutdown/hypnic-shutdown.py" https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic-shutdown.py
-chmod 755 /usr/bin/hypnic.py
-chmod 755 "$DIR/system-shutdown/hypnic-shutdown.py"
+if [[ "$MODE" == "pcp" ]]; then
+  DEST_FILE=/etc/sysconfig/tcedir/optional/hypnic.tcz
+  wget -qO $DEST_FILE https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/pcp/hypnic.tcz
+  echo "hypnic.tcz" >> /etc/sysconfig/tcedir/onboot.lst
+  tce-load -i $DEST_FILE
+fi
 
-echo "Configuring service..."
-systemctl daemon-reload
-systemctl enable hypnic.service
-systemctl start hypnic.service
+if [[ "$MODE" == "systemd" ]]; then
+  echo "Installing required libraries..."
+  DEBIAN_FRONTEND=noninteractive apt-get install -qq python3-dev python3-rpi.gpio < /dev/null > /dev/null
+
+  echo "Downloading and installing application..."
+  wget -qO /etc/hypnic.conf https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.conf
+  wget -qO /usr/bin/hypnic.py https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.py
+  wget -qO "$DIR/system/hypnic.service" https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic.service
+  wget -qO "$DIR/system-shutdown/hypnic-shutdown.py" https://raw.githubusercontent.com/gilphilbert/hypnic/main/sbc/hypnic-shutdown.py
+  chmod 755 /usr/bin/hypnic.py
+  chmod 755 "$DIR/system-shutdown/hypnic-shutdown.py"
+
+  echo "Configuring service..."
+  systemctl daemon-reload
+  systemctl enable hypnic.service
+  systemctl start hypnic.service
+fi
+
+
 echo ""
 echo "Installation complete. The default pins are:"
 echo "  HALT: GPIO17"
